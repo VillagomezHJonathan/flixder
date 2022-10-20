b349bb8ef55dd3c150555c88bbb7429a2f21750f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-10-20 18:53:32 -0400

    Added async function that gets movies with passed in arguments for the front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useEffect } from 'react'
+import { DOMAIN } from './globals'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const getMoviesAsync = async (region, providers, genres) => {
+      const res = await axios.get(
+        `${DOMAIN}/discover/movie?watch_region=${region}&with_watch_providers=${providers}&with_genres=${genres}&api_key=${process.env.REACT_APP_TMDB_KEY}`
+      )
+      console.log(res.data.results)
+    }
+
+    getMoviesAsync('US', '8|9|337', '28,12,16')
+  })
+
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
---
+++ client/src/globals/index.js
@@ -0,0 +1,2 @@
+export const DOMAIN = 'https://api.themoviedb.org/3'
+export const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
