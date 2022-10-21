70bc33f32a0cb9bf969de829e7540dfcff761660
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-10-21 10:45:12 -0400

    Finished genre routes and controller

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const apiRoutes = require('./routes/api')
+const db = require('./db')
+const genreRoutes = require('./routes/genre')
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.use('/api', apiRoutes)
+app.use('/genres', genreRoutes)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
