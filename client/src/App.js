430f9cfcde32d5db601f8ae22858f75cd3fe0234
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-10-21 16:15:07 -0400

    Modified profile model to include a profile pi and made the skeleton of 2 components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,6 @@
 import './App.css'
-import { useEffect } from 'react'
-import { DOMAIN } from './globals'
-import axios from 'axios'
 
 function App() {
-  useEffect(() => {
-    const getMoviesAsync = async (region, providers, genres) => {
-      const res = await axios.get(
-        `${DOMAIN}/discover/movie?watch_region=${region}&with_watch_providers=${providers}&with_genres=${genres}&api_key=${process.env.REACT_APP_TMDB_KEY}`
-      )
-      console.log(res.data.results)
-    }
-
-    getMoviesAsync('US', '8|9|337', '28,12,16')
-  })
-
   return <div className="App"></div>
 }
 
