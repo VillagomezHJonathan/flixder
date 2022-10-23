9fd30f195088194f629cb2e4ac960727b3fa9b75
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-10-23 16:08:02 -0400

    Added routes directory

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import './App.css'
 import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import NavBar from './components/NavBar'
-import Profile from './components/Profile'
-import Home from './components/Home'
-import NewProfile from './components/NewProfile'
+import NavBar from './components/routes/NavBar'
+import Profile from './components/routes/Profile'
+import Home from './components/routes/Home'
+import NewProfile from './components/routes/NewProfile'
 
 function App() {
   const [currentProfile, setCurrentProfile] = useState({})
