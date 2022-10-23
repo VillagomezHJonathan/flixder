08abf03a4406f82e3a46ff63e4b497705f496af1
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-10-22 20:46:41 -0400

    Added getData function that gets everything necessary to make a profile

--- client/src/components/NewProfile.jsx
+++ client/src/components/NewProfile.jsx
@@ -1,9 +1,28 @@
 import './NewProfile.css'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const NewProfile = () => {
+  const [regions, setRegions] = useState([])
+  const [providers, setProviders] = useState([])
   const [images, setImages] = useState([])
   const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const regionsRes = await axios.get('http://localhost:3001/regions')
+      const providersRes = await axios.get('http://localhost:3001/providers')
+      const imagesRes = await axios.get('http://localhost:3001/images')
+      const genresRes = await axios.get('http://localhost:3001/genres')
+
+      setRegions(regionsRes.data.regions)
+      setProviders(providersRes.data.providers)
+      setImages(imagesRes.data.images)
+      setGenres(genresRes.data.genres)
+    }
+
+    getData()
+  }, [])
 
   return (
     <div className="NewProfile">
