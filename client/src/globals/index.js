7cff31aa33c543dffafc6f685ae6fa00651f5c3b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-11-02 00:35:09 -0400

    Fixed server

--- client/src/globals/index.js
+++ client/src/globals/index.js
@@ -1,8 +1,5 @@
 export const DOMAIN = 'https://api.themoviedb.org/3'
 export const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
 
-let DB_BASE_URL = ''
-
-process.env.NODE_ENV === 'production'
-  ? (DB_BASE_URL = 'http://localhost:3001')
-  : ''
+export let DB_BASE_URL =
+  process.env.NODE_ENV === 'production' ? 'http://localhost:3001' : ''
